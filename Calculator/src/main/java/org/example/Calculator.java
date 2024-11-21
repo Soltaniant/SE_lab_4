6fdc94f35d38e6c65f62350dcee61a456b9234c4
mousavi1382 <me.mousavi1382@gmail.com>
2024-11-21 23:20:11 +0330

    add divide function

--- Calculator/src/main/java/org/example/Calculator.java
+++ Calculator/src/main/java/org/example/Calculator.java
@@ -4,4 +4,11 @@ public class Calculator {
 	public double add(double a, double b) {
 		return a + b;
 	}
+
+	public double divide(double a, double b) {
+		if (b == 0) {
+			throw new IllegalArgumentException("Division by zero is not allowed");
+		}
+		return a / b;
+	}
 }
